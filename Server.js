5e7e431715ab23cdf7a21b0e1eac5db17f8b0cfa
lagia <lagia@LAPTOP-8U4NVVVF.hfu-open.fu.hs-furtwangen.de>
2018-05-12 19:58:27 +0200

    Server

--- Server.js
+++ Server.js
@@ -1,30 +1,30 @@
 "use strict";
 const Http = require("http");
 const Url = require("url");
-var Node;
-(function (Node) {
-    let port = process.env.PORT; //process.env.PORT wird als Umgebungsvar festgelegt f�r den gegebenen PORT
+var Server;
+(function (Server) {
+    let port = process.env.PORT;
     if (port == undefined)
-        port = 8100; //dann port = 8100
-    let server = Http.createServer(); //eigenen server creieren
-    server.addListener("listening", handleListen); //wenn server 'listening dann function handleListen aufrufen
-    server.addListener("request", handleRequest); //sever 'requeste' reagieren beibringen
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
     server.listen(port);
     function handleListen() {
-        console.log("Ich h�re?"); //ausgabe in console oder terminal
+        console.log("Ich höre?");
     }
     function handleRequest(_request, _response) {
-        console.log("Ich h�re Stimmen!");
-        let query = Url.parse(_request.url, true).query; //�bersetzung in assoziatives array und umwandeln in js (/?a=10&b=20)
+        console.log("Ich höre Stimmen!");
+        let query = Url.parse(_request.url, true).query;
         let a = parseInt(query["a"]);
         let b = parseInt(query["b"]);
+        for (let key in query)
+            console.log(query[key]);
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("Ich habe dich geh�rt<br/>");
-        for (let key in query)
-            _response.write("Die eingegebenen Query-Informationen: " + (query[key]) + "<br>");
-        _response.write("Das Ergebnis lautet:" + (a + b));
+        _response.write("Ich habe dich gehört<br/>");
+        _response.write("Das Ergebnis ist: " + (a + b));
         _response.end();
     }
-})(Node || (Node = {}));
+})(Server || (Server = {}));
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -1,39 +1,40 @@
 import * as Http from "http";
-import * as Url from "url"; 
+import * as Url from "url";
 
-namespace Node {
+namespace Server {
     interface AssocStringString {
         [key: string]: string;
     }
 
-    let port: number = process.env.PORT; //process.env.PORT wird als Umgebungsvar festgelegt für den gegebenen PORT
-    if (port == undefined)//wenn undefinded 
-        port = 8100;//dann port = 8100
-    
-    let server: Http.Server = Http.createServer();//eigenen server creieren
-    server.addListener("listening", handleListen);//wenn server 'listening dann function handleListen aufrufen
-    server.addListener("request", handleRequest);//sever 'requeste' reagieren beibringen
+    let port: number = process.env.PORT;
+    if (port == undefined)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer();
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
     server.listen(port);
 
     function handleListen(): void {
-        console.log("Ich höre?");//ausgabe in console oder terminal
+        console.log("Ich hÃ¶re?");
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("Ich höre Stimmen!");
+        console.log("Ich hÃ¶re Stimmen!");
 
-        let query: AssocStringString = Url.parse(_request.url, true).query;//übersetzung in assoziatives array und umwandeln in js (/?a=10&b=20)
+        let query: AssocStringString = Url.parse(_request.url, true).query;
         let a: number = parseInt(query["a"]);
         let b: number = parseInt(query["b"]);
 
+        for (let key in query) 
+            console.log(query[key]);
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("Ich habe dich gehört<br/>");
 
-        for (let key in query)//Schlüsseldurchgabe
-        _response.write("Die eingegebenen Query-Informationen: " + (query[key]) + "<br>");
-        _response.write("Das Ergebnis lautet:" + (a + b));
+        _response.write("Ich habe dich gehÃ¶rt<br/>");
+        _response.write("Das Ergebnis ist: " + (a + b));
+
         _response.end();
     }
-} 
+}
