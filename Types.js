93341f1dcaeb838ea2625498c4b2c6ac23acf4ea
lagia <lagia@LAPTOP-8U4NVVVF.eduroam.hs-furtwangen.de>
2018-06-14 12:01:40 +0200

    8

---
+++ Types.js
@@ -0,0 +1,4 @@
+// Struktur des heterogenen assoziativen Arrays als Datensatz f�r eine studierende Person
+// Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+let studiHomoAssoc = {};
+//# sourceMappingURL=Types.js.map
---
+++ Types.ts
@@ -0,0 +1,36 @@
+
+
+// Struktur des heterogenen assoziativen Arrays als Datensatz für eine studierende Person
+
+interface Studi {
+
+    name: string;
+
+    firstname: string;
+
+    matrikel: number;
+
+    age: number;
+
+    gender: boolean;
+
+    studiengang: string;
+
+}
+
+
+
+// Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+
+interface Studis {
+    [matrikel: string]: Studi;
+
+}
+
+
+
+// Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+
+let studiHomoAssoc: Studis = {};
+
+
